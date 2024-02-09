144805a6d7a25ad46c502c2738846f10d84ecec8
mitesh <ShriMiTesHcHauDHary@gmail.com>
2024-02-09 17:31:57 +0530

    9-2-24

---
+++ arrfn5.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5,6,7,2,9,10]
+let naam = "miteshchaudhary"
+
+const huihui1 = arr.join(" ")
+console.log(huihui1);
+
+const huihui2 = naam.split(",")
+console.log(huihui2);
+
+
+const huihui3 = arr.at(0)
+console.log(huihui3);
+
+const huihui4 = arr.find((x)=>x>2)
+console.log(huihui4);
